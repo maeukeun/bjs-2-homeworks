f88ee2917794e72e9f3bc44a33d899b7d49894af
Petrov Maxim <maxim.petrov@1ci.com>
2023-10-10 08:13:49 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,91 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum = sum + arr[i];
+	}
+	const avg = Number((sum / arr.length).toFixed(2));
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
 function summElementsWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum = sum + arr[i]
+	}
+	return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let min = arr[0];
+	let max = arr[0];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	let difference = max - min;
+	return difference;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			sumEvenElement = sumEvenElement + arr[i];
+		} else {
+			sumOddElement = sumOddElement + arr[i];
+		}
+	}
+	return (sumEvenElement - sumOddElement);
 }
 
 function averageEvenElementsWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			sumEvenElement = sumEvenElement + arr[i];
+			countEvenElement++;
+		}
+	}
+	return (sumEvenElement / countEvenElement);
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		const funcResult = func(...arrOfArr[i]);
+		if (maxWorkerResult < funcResult) {
+			maxWorkerResult = funcResult;
+		}
+	}
+	return maxWorkerResult;
 }
