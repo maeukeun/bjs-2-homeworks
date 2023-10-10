3e90eec24cce698ab0fd7ea663056028e957b888
Petrov Maxim <maxim.petrov@1ci.com>
2023-10-10 09:21:32 +0300

    Codestyle

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,32 +1,32 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marksToAdd) {
-    if (this.marks === undefined) {
-        console.log("Студент отчислен");
-        return;
-      }
-      this.marks.push(...marksToAdd);
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (this.marks === undefined) {
+		console.log("Студент отчислен");
+		return;
+	}
+	this.marks.push(...marksToAdd);
 }
 
-Student.prototype.getAverage = function () {
-    if (this.marks === undefined || this.marks.length === 0) {
-        return 0;
-      }
-      const sum = this.marks.reduce((total, mark) => total + mark, 0);
-      return sum / this.marks.length;
+Student.prototype.getAverage = function() {
+	if (this.marks === undefined || this.marks.length === 0) {
+		return 0;
+	}
+	const sum = this.marks.reduce((total, mark) => total + mark, 0);
+	return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
