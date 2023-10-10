2136dd86877b00c4f1f771fcd8d3af0364f3928f
Petrov Maxim <maxim.petrov@1ci.com>
2023-10-10 08:46:57 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	return arr1.every((value, index) => {
+		return value === arr2[index];
+	})
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filtredUsers = users.filter(user => user.gender === gender);
+	if (filtredUsers.length === 0) {
+		return 0;
+	}
+
+	const totalAge = filtredUsers.reduce((acc, user) => acc + user.age, 0);
+	const averageAge = totalAge / filtredUsers.length;
+	return averageAge;
 }
